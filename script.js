ff95e06cf1ad3f057a468693c6fece1837739b45
dkrishna11 <116519809+dkrishna11@users.noreply.github.com>
2023-05-26 14:56:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let h1=document.getElementsByTagName("h1")[0];
+
+let width=window.innerWidth;
+let height=window.innerHeight;
+
+h1.innerText="Width:" +this.innerWidth+" and Height: "+this.innerHeight;
